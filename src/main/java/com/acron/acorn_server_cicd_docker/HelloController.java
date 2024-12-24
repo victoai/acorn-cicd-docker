74956d82b1175c1710f5edc23ab0ddb66ffa44a7
victoai <victoai999@gmail.com>
2024-12-24 14:17:03 +0900

    cicd

---
+++ src/main/java/com/acron/acorn_server_cicd_docker/HelloController.java
@@ -0,0 +1,16 @@
+package com.acron.acorn_server_cicd_docker;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hi")
+
+    public  String hi(){
+        return  "hi docker cicd";
+    }
+}
