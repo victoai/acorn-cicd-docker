2ec8864b428f20d71b0a25d637de5d662febca05
victoai <victoai999@gmail.com>
2024-12-24 14:03:09 +0900

    dfdf

---
+++ src/main/java/com/acron/acorn_server_cicd_docker/AcornServerCicdDockerApplication.java
@@ -0,0 +1,13 @@
+package com.acron.acorn_server_cicd_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornServerCicdDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornServerCicdDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acron/acorn_server_cicd_docker/AcornServerCicdDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acron.acorn_server_cicd_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcornServerCicdDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
